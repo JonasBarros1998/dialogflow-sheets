6ef4a29206c700cc8a930b89135c6e0f181571cc
Jonas Barros <jonas.barros@multiedro.com.br>
2021-02-08 21:15:01 -0300

    configurando o eslint

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get('/', function(request, response){ 
-    response.json({message: "Inicio do projeto"});
+app.get('/', function(request, response) {
+  response.json({message: 'Inicio do projeto'});
 });
 
 app.listen(3000);
