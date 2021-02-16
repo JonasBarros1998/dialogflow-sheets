d0e9790ba067806806c7afa718d526419249689f
Jonas Barros <jonas.barros@multiedro.com.br>
2021-02-16 10:52:44 -0300

    Criando classe para receber dados do google sheets

--- src/core/infra/http/server.ts
+++ src/core/infra/http/server.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import {router, receiverRouter} from './routes/index';
+// eslint-disable-next-line max-len
+// import GoogleAuthentication from '../../../modules/authentication/GoogleAuthentication';
 
 const app = express();
 const port = 3333;
@@ -10,6 +12,9 @@ app.use(express.json());
 
 app.listen(port, function() {
   console.log('server address: http://localhost:3333');
+  // const googleAuthentication = new GoogleAuthentication();
+  // const auth = googleAuthentication.auth();
+  // googleAuthentication.listMajors(auth);
 });
 
 export default app;
---
+++ src/modules/googleSheets/domain/GoogleSheets.ts
@@ -0,0 +1,5 @@
+class GoogleSheets {
+  
+}
+
+export default GoogleSheets;
