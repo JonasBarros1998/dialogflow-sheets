a26378d43a6d3d559d2fef6761b214cd8f517d0a
Jonas Barros <jonas_barros@outlook.com>
2021-02-20 17:58:37 -0300

    Adicionando os controladores nas rotas

--- src/core/infra/http/routes/receiver/Receiver.route.ts
+++ src/core/infra/http/routes/receiver/Receiver.route.ts
@@ -1,10 +1,15 @@
 import express from 'express';
+import
+ReceiverController
+  from '../../../../controllers/receiver/ReceiverController';
 
 // eslint-disable-next-line new-cap
 const receiverRouter = express.Router();
 
-receiverRouter.get('/receiver', function(request, response) {
-  return response.json({message: 'route: /receiver'});
+receiverRouter.get('/receiver', async function(request, response) {
+  const receiverController = new ReceiverController(request, response);
+  const receiverGoogleSheets = await receiverController.receiver();
+  return response.json(receiverGoogleSheets);
 });
 
 export {receiverRouter};
--- src/core/infra/http/routes/send/send.route.ts
+++ src/core/infra/http/routes/send/send.route.ts
@@ -1,10 +1,21 @@
-import express from 'express';
+import {Router} from 'express';
+import env from '../../../../../../env.json';
+import SendController from '../../../../controllers/send/SendController';
 
 // eslint-disable-next-line new-cap
-const router = express.Router();
+const router = Router();
 
-router.get('/send', function(request, response) {
-  return response.json({message: 'route: /send/google-sheets'});
+router.post('/send', async function(request, response) {
+  request.body = {
+    'values': [[request.body.queryResult.queryText]],
+    'range': 'A1',
+    'spreedSheetId': env.sheet.spreadsheet_id,
+    'majorDimension': 'ROWS',
+    'valueInputOption': 'RAW',
+  };
+  const sendController = new SendController(request, response);
+  const responseGoogleSheets = await sendController.sendGoogleSheets();
+  return response.json(responseGoogleSheets);
 });
 
 export default router;
