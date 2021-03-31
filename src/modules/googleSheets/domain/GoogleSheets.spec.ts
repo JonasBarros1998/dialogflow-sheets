196487edc4216d93479b6d869d9071c9a8cb1f1c
Jonas Barros <jonas_barros@outlook.com>
2021-03-30 22:19:59 -0300

    refatorando a classe de dominio

--- src/modules/googleSheets/domain/GoogleSheets.spec.ts
+++ src/modules/googleSheets/domain/GoogleSheets.spec.ts
@@ -2,16 +2,48 @@ import GoogleSheets from '../../googleSheets/domain/GoogleSheets';
 import GoogleAuthentication from '../../authentication/GoogleAuthentication';
 
 describe('google sheets', function() {
-  it('get datas google sheets', async function() {
-    const googleAuthentication = new GoogleAuthentication();
-    const auth = googleAuthentication.auth();
-    const googleSheets = await GoogleSheets.getSheets(auth);
-    expect(googleSheets).toEqual(
-        expect.objectContaining({
-          range: expect.any(String),
-          majorDimension: expect.any(String),
-          values: expect.any(Array),
-        }),
-    );
+  const data = {
+    values: [
+      ['nome: jonas, telefone: (00) 900000-1111'],
+    ],
+    range: 'A1',
+    spreadsheetId: '1234',
+    majorDimension: 'ROWS',
+    valueInputOption: 'RAW',
+  };
+
+  // it('get datas google sheets', async function() {
+  //   const googleAuthentication = new GoogleAuthentication();
+  //   const auth = googleAuthentication.auth();
+  //   const googleSheets = await GoogleSheets.getSheets(auth);
+  //   expect(googleSheets).toEqual(
+  //       expect.objectContaining({
+  //         range: expect.any(String),
+  //         majorDimension: expect.any(String),
+  //         values: expect.any(Array),
+  //       }),
+  //   );
+  // });
+
+  it('Method sheets is returned typeof a object', function() {
+    const googleSheets = new GoogleSheets(data);
+    const dataSheets = typeof googleSheets.sheets();
+    expect(dataSheets).toBe('object');
+  });
+
+  it('Method sheets is return a object with the correct properties', function() {
+    const googleSheets = new GoogleSheets(data);
+    const dataSheets = googleSheets.sheets();
+    dataSheets.map((data)=> {
+      expect(data).toEqual(
+          expect.objectContaining({
+            values: expect.any(Array),
+            range: expect.any(String),
+            spreadsheetId: expect.any(String),
+            majorDimension: expect.any(String),
+            valueInputOption: expect.any(String),
+          }),
+      );
+    });
   });
 });
--- src/modules/googleSheets/domain/GoogleSheets.ts
+++ src/modules/googleSheets/domain/GoogleSheets.ts
@@ -1,12 +1,31 @@
-/* eslint-disable max-len */
 /* eslint-disable camelcase */
 import {google, Auth, sheets_v4} from 'googleapis';
 import env from '../../../../env.json';
+import {IDatasGoogleSheets} from '../interface/IDatasGoogleSheets';
 
-/**
- * @class
- */
 class GoogleSheets {
+  private googleSheets: IDatasGoogleSheets
+  private dataGoogleSheets: Array<IDatasGoogleSheets>
+  /**
+   * @constructor
+   * @param {IDatasGoogleSheets} googleSheets
+   */
+  constructor(googleSheets: IDatasGoogleSheets) {
+    this.googleSheets = googleSheets;
+    this.dataGoogleSheets = [];
+  }
+
+  /**
+   * Retorna um array com todos os atributos necessários para
+   * adicionar os dados ao google sheets.
+   * @method
+   * @return {Array<IDatasGoogleSheets>}
+   */
+  public sheets():Array<IDatasGoogleSheets> {
+    this.dataGoogleSheets.push(this.googleSheets);
+    return this.dataGoogleSheets;
+  }
+
   /**
    * Retorna os dados cadastrados em uma planilha do google sheets
    * @static
