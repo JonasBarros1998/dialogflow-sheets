21b66516ec2d16fa9312cf65f18fcbd93d5955af
Jonas Barros <jonas_barros@outlook.com>
2021-04-02 17:42:01 -0300

    Criando interface as propriedades das credenciais auth

---
+++ src/modules/authentication/interfaces/IData.ts
@@ -0,0 +1,13 @@
+/* eslint-disable camelcase */
+
+export interface IData {
+  web: {
+    client_id: string;
+    project_id: string;
+    auth_uri: string;
+    token_uri: string;
+    auth_provider_x509_cert_url: string;
+    client_secret: string;
+    redirect_uris: Array<string>;
+  }
+};
