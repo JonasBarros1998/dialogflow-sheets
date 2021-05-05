2d06f4b43ec35f90eaf13cbd3d44c0dc75684711
Jonas Barros <jonas_barros@outlook.com>
2021-05-05 12:00:05 -0300

    Desenvolvendo o controlador. Metodos para validação de requisição.

--- src/adapters/controllers/send/Send.ts
+++ src/adapters/controllers/send/Send.ts
@@ -1,3 +1,4 @@
+import {Request, response, Response} from 'express';
 import {IAddClient} from '../../../useCase/interface/iadd-client';
 import AddClient from '../../../useCase/add-client';
 import {IDataBase} from '../../gateway/IDataBase';
@@ -5,17 +6,54 @@ import DataBase from '../../../external/db/DataBase';
 import {IClient} from '../../../entity/interfaces/IClient';
 
 class Send {
-  private addClient: IAddClient;
   private database: IDataBase;
+  private body: IClient;
+  private newClient: AddClient
   constructor(private request: Request, private response: Response) {
     this.database = new DataBase();
+    this.body = this.request.body;
+    this.newClient = new AddClient(this.body, this.database);
   }
 
   sendData() {
-    if (typeof this.request.body !== null) {
-      const body: IClient = this.request.body; 
-      this.addClient = new AddClient(body, this.database);
+    const requestIsValid = this.validRequest(this.body);
+    if (requestIsValid !== true) {
+      return requestIsValid;
     }
+    return this.newClient.toAdd()
+        .then(function(response) {
+          console.log(response);
+        })
+        .catch((error) => {
+          return this.response.status(500).send(error);
+        });
+  }
+
+  private validRequest(body: IClient) {
+    const objectIsEmpty = this.emptyObject(body);
+    if (objectIsEmpty.status === false) {
+      return this.response.status(500).json(objectIsEmpty);
+    }
+    const bodyIsObject = this.typeofObjectObdy(body);
+    if (bodyIsObject.status === false) {
+      return this.response.status(500).json(bodyIsObject);
+    }
+    return true;
+  }
+
+  private emptyObject(body: object) {
+    const keys = Object.keys(body);
+    if (keys.length === 0) {
+      return {status: false, message: `The object don't can empty`};
+    }
+    return {status: true, message: 'sucess'};
+  }
+
+  private typeofObjectObdy(body: object) {
+    if (typeof body !== 'object') {
+      return {status: false, message: `the body don't is object. typeof ${typeof body}`};
+    }
+    return {status: true, message: 'sucess'};
   }
 }
 
--- src/core/infra/http/routes/send/send.route.ts
+++ src/core/infra/http/routes/send/send.route.ts
@@ -1,20 +1,22 @@
 import {Router} from 'express';
 import env from '../../../../../../env.json';
 import SendController from '../../../../controllers/send/SendController';
+import Send from '../../../../../adapters/controllers/send/Send';
 
 const router = Router();
 
 router.post('/send', async function(request, response) {
-  request.body = {
-    'values': [[request.body.queryResult.queryText]],
-    'range': 'A1',
-    'spreadsheetId': env.sheet.spreadsheet_id,
-    'majorDimension': 'ROWS',
-    'valueInputOption': 'RAW',
-  };
-  const sendController = new SendController(request, response);
-  const responseGoogleSheets = await sendController.sendGoogleSheets();
-  return response.json(responseGoogleSheets);
+  // request.body = {
+  //   'values': [[request.body.queryResult.queryText]],
+  //   'range': 'A1',
+  //   'spreadsheetId': env.sheet.spreadsheet_id,
+  //   'majorDimension': 'ROWS',
+  //   'valueInputOption': 'RAW',
+  // };
+  // const sendController = new SendController(request, response);
+  // const responseGoogleSheets = await sendController.sendGoogleSheets();
+  const send = new Send(request, response);
+  return send.sendData();
 });
 
 export default router;
