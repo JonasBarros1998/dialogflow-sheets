88d49a3b6825892703089191e31030367af0c343
Jonas Barros <jonas_barros@outlook.com>
2021-05-17 22:16:12 -0300

    Alterando o metodo Send para facilitar no desenvolvimento dos testes unitarios

--- src/adapters/controllers/interface/IControllerValidate.ts
+++ src/adapters/controllers/interface/IControllerValidate.ts
@@ -4,5 +4,5 @@ import {IMessageController} from '../../../shared/interfaces/IMessageController'
 export interface IControllerValidate {
   _emptyObject(body: object): IMessageController
   _typeofObject(body: object): IMessageController
-  _validateRequest(body: IClient): IMessageController|boolean
+  _validateRequest(body: IClient): IMessageController
 }
