f71b5189aa3298cf4e0501fd7eb3694c3b806efb
Jonas Barros <jonas_barros@outlook.com>
2021-05-21 22:07:15 -0300

    Foram retirados metodos de desse controlador. Alterando os parametros que são recebido no construtor Utilizando destructor na atribuição de variaveis

--- src/adapters/controllers/send/Send.ts
+++ src/adapters/controllers/send/Send.ts
@@ -1,37 +1,28 @@
-import {Response} from 'express';
+import {Response, Request} from 'express';
 import AddClient from '../../../useCase/add-client';
 import {IDataBase} from '../../gateway/IDataBase';
 import DataBase from '../../../external/db/DataBase';
-import {IClient} from '../../../entity/interfaces/IClient';
-import {IControllerValidate} from '../interface/IControllerValidate';
-import {IMessageController} from '../../../shared/interfaces/IMessageController';
+import {createClient} from './create-body-object';
 import {IResponse} from '../interface/IResponse';
 import ResponseController from '../Response/Response';
+import ValidationsController from '../Validations/ValidationsController';
 
-class Send implements IControllerValidate {
+class Send {
   private database: IDataBase;
-  private body: any;
   private responseController: IResponse;
-  constructor(body: any, private response: Response) {
-    this.body = body;
+  constructor(private req: Request, private res: Response) {
     this.database = new DataBase();
-    this.responseController = new ResponseController(this.response);
+    this.responseController = new ResponseController(this.res);
   }
 
   sendData() {
-    const requestIsValid = this._validateRequest(this.body);
-    if (requestIsValid.status !== true) {
+    const validRequest = new ValidationsController();
+    const {status, statusCode, message} = validRequest.request(this.req);
+    if (status !== true) {
       return this.responseController
-          .send(requestIsValid.statusCode,
-              {status: requestIsValid.status,
-                message: requestIsValid.message});
+          .send(statusCode, {status, message});
     }
-    const client: IClient = {
-      name: this.body.name,
-      description: this.body.description,
-      email: this.body.email,
-      phone: this.body.phone,
-    };
+    const client = createClient(this.req);
     const addClient = new AddClient(client, this.database);
     return addClient.toAdd()
         .then((result) => {
@@ -43,37 +34,6 @@ class Send implements IControllerValidate {
                   {status: false,
                     message: error.message});
         });
-  }
-
-  _validateRequest(body: IClient): IMessageController {
-    const objectIsEmpty = this._emptyObject(body);
-    if (objectIsEmpty.status === false) {
-      return objectIsEmpty;
-    }
-    const bodyIsObject = this._typeofObject(body);
-    if (bodyIsObject.status === false) {
-      return bodyIsObject;
-    }
-    return {status: true, message: 'sucess', statusCode: 200};
-  }
-
-  _emptyObject(body: object): IMessageController {
-    const keys = Object.keys(body);
-    if (keys.length === 0) {
-      return {status: false,
-        message: `The object is empty`,
-        statusCode: 400};
-    }
-    return {status: true, message: 'sucess', statusCode: 200};
-  }
-
-  _typeofObject(body: object): IMessageController {
-    if (typeof body !== 'object') {
-      return {status: false,
-        message: `the body don't is object. typeof ${typeof body}`,
-        statusCode: 400};
-    }
-    return {status: true, message: 'sucess', statusCode: 200};
   }
 }
 
