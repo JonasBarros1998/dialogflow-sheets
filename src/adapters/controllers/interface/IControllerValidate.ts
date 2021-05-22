7987fa1a258b7021892a1147d80073788a844546
Jonas Barros <jonas_barros@outlook.com>
2021-05-21 22:11:34 -0300

    Deletando a classe IControllerValidade

--- src/adapters/controllers/interface/IControllerValidate.ts
+++
@@ -1,8 +0,0 @@
-import {IClient} from '../../../entity/interfaces/IClient';
-import {IMessageController} from '../../../shared/interfaces/IMessageController';
-
-export interface IControllerValidate {
-  _emptyObject(body: object): IMessageController
-  _typeofObject(body: object): IMessageController
-  _validateRequest(body: IClient): IMessageController
-}
