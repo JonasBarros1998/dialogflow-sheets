cc5c0b70a27931f82d4dbc78aabc88140c578d6e
Jonas Barros <jonas_barros@outlook.com>
2021-05-21 22:08:57 -0300

    Criando classe para Response para envio de resposta ao consumidor da API

--- src/adapters/controllers/Response/Response.ts
+++ src/adapters/controllers/Response/Response.ts
@@ -2,12 +2,9 @@ import {IResponse} from '../interface/IResponse';
 import {Response} from 'express';
 
 class ResponseController implements IResponse {
-  constructor(private response: Response) {}
-
-  send(statusCode: number, json: any): Response<any, Record<string, any>> {
-    return this.response
-        .status(statusCode)
-        .json(json);
+  constructor(private res: Response) {}
+  send(statusCode: number, json: any): any {
+    return this.res.status(statusCode).json(json);
   }
 }
 
