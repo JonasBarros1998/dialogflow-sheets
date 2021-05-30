badb577bb1a790e9d4f088962c2216998a971db2
Jonas Barros <jonas_barros@outlook.com>
2021-05-30 15:51:24 -0300

    Deletando arquivos não utilizados

--- start.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-docker build -f Dockerfile -t jonasflorencio/dialogflow-sheets --target developer .
-
-docker run --name test -v $(pwd):/home/app -p 8080:8080 jonasflorencio/dialogflow-sheets
-
-docker rm -f test
