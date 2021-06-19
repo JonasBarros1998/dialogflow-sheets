ac59414d6d5f86f2e49a6d614509565d98e2bfb9
Jonas Barros <jonas_barros@outlook.com>
2021-06-19 10:21:28 -0300

    desenvolvendo listagem de clientes

--- src/external/http/routes/send.route.ts
+++ src/external/http/routes/send.route.ts
@@ -3,9 +3,15 @@ import RouterAdapter from '../../../main/adapters/RouterAdapter';
 
 const router = Router();
 
-router.post('/send', async function(request: Request, response: Response) {
+router.post('/client', async function(request: Request, response: Response) {
   const routerAdapter = new RouterAdapter();
   const router = await routerAdapter.send(request, response);
+  return router;
+});
+
+router.get('/client', async function(request: Request, response: Response) {
+  const routerAdapter = new RouterAdapter();
+  const router = await routerAdapter.list(request, response);
   return router;
 });
 
